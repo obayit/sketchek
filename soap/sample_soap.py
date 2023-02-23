8514bfdb336cba97fa9698049cd2ad4487ae549f
Obay Abdelgadir <obayit@gmail.com>
2023-02-23 11:29:48 +0200

    [sync]

--- soap/sample_soap.py
+++ soap/sample_soap.py
@@ -7,13 +7,9 @@ SOAP_URL = 'http://www.dneonline.com/calculator.asmx?WSDL'
 
 def do_stuff():
     client = ZeepClient(SOAP_URL)
-    requestParams = {
-        'intA': 1,
-        'intB': 1,
-    }
     try:
-        print(requestParams)
-        result = client.Add.getCitizenInfo(requestParams)
+        result = client.service.Add(1, 1)
+        print('result')
         print(result)
     except Fault as ex:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
@@ -23,5 +19,5 @@ def do_stuff():
         print(ex)
     return
 
-
+do_stuff()
 
